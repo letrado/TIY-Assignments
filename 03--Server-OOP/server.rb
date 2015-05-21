d2da593250b039bb398536b6896f360bb7cb232f
Mark Sadler <msadler10@hotmail.com>
2015-05-21 07:54:05 -0400

    Improve Goodbye Message

--- 03--Server-OOP/server.rb
+++ 03--Server-OOP/server.rb
@@ -9,7 +9,7 @@ end
 
 
 server.mount_proc "/shutdown" do |request, response|
-  response.body = "Bye"
+  response.body = "Jajatopata, Auf widersehen, Au revoir, Ciao, Hasta Pronto, Ate Logo, Goodbye, etc."
   server.shutdown
 end
 
