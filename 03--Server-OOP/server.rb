a28edd66ded7fd612acbff8b18578c9402218982
Mark Sadler <msadler10@hotmail.com>
2015-05-20 23:42:21 -0400

    Create Basic Server

--- 03--Server-OOP/server.rb
+++ 03--Server-OOP/server.rb
@@ -1,1 +1,16 @@
+require "webrick"
+require "erb"
 
+server = WEBrick::HTTPServer.new(:Port => 8000)
+
+server.mount_proc "/home" do |request, response|
+  response.body= File.read "home.html"
+end
+
+
+server.mount_proc "/shutdown" do |request, response|
+  response.body = "Bye"
+  server.shutdown
+end
+
+server.start
