73eaf55798b2e36e645a71ddade016154cd3b489
Mark Sadler <msadler10@hotmail.com>
2015-06-07 20:41:53 -0400

    update with edit and delete functions

--- first_blog/app/controllers/posts_controller.rb
+++ first_blog/app/controllers/posts_controller.rb
@@ -22,9 +22,18 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :author, :content))
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
   
 
